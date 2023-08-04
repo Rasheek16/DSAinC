c5592a861e8a3a3b4d21c8a2fe4ddc4129dbc555
Rasheek <131476157+Rasheek16@users.noreply.github.com>
2023-08-05 00:17:39 +0530

    Linear Search in C language

---
+++ SearchingAlgorithms/LinearSearch.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int linearSearch(int A[], int elementCount, int searchElement){
+    printf("Enter element to search: ");
+    scanf("%d",&searchElement);
+    for (int x=0; x<(sizeof(A)/sizeof(A[0])); x++){
+        if (A[x]==searchElement){
+           return x;
+        };
+
+    }
+};;
+int main(){
+    int elementCount;
+    printf("Enter number of elements to be inserted in the array :");
+    scanf("%d",&elementCount);
+    int elements;
+    int searchElement;
+    int A[elementCount];
+    for (int i=0; i<elementCount; i++){
+        printf("Enter element : ");
+        scanf("%d",&A[i]);
+    }
+    int result = linearSearch(A,elementCount,searchElement);
+    if (result) {
+        printf("Element found at index: %d\n", result+1);
+    } else {
+        printf("Element not found\n");
+    }
+
+    return 0;
+}
